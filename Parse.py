486d0d7e296e6c47755e3d6705dd19b689ffcf61
Pierce Dreiling <piercedreiling@gmail.com>
2023-10-12 17:54:15 -0500

    added parsing functions for table, attributes, and fds

--- Parse.py
+++ Parse.py
@@ -1,6 +1,76 @@
-def csv_parse(filename: str):
-    return None
+import csv
+import Table as T
+import Attribute as A
+import FunctionalDependency as FD
+
+""" Parses the relation in the .csv file to get attributes
+    Input: .csv file
+    Output: list of Attribute objects
+"""
+def csvParse(filename: str) -> list[A.Attribute]:
+    attributes: list[A.Attribute] = []
+
+    with open(filename, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        for attribute in reader[0]: # getting each column attribute from first row of .csv file
+            attributes.append(attribute)
+        csv_file.close()
+
+    return attributes
+
+""" Takes inputted key, parses it into a set
+    Input: string indication primary key of inputted table
+    Output: set of strings indicating prime attributes
+"""
+def keyParse(key: str) -> set[str]:
+    key_set: set[str] = set()
+    parsed_key = [x.strip() for x in key.split(',')]
+    for k in parsed_key:
+        key_set.add(k)
+    return key_set
 
 
-def fd_parse():
-    return None
+""" Parse each inputted functional dependency (strings) into FD objects
+    Input: list of inputted strings representing FDs
+    Output: list of FD objects representing each specified FD for input relation
+"""
+def fdParse(fd_list: list[str], primary_key: set[str]) -> list[FD.FunctionalDependency]:
+    partitioned_fd: tuple[str, str, str] = tuple()
+    lhs: list[str] = [] # left side of fd
+    rhs: list[str] = [] # right side of fd
+    determinants: set[A.Attribute] = set()
+    non_determinants: set[A.Attribute] = set()
+    parsed_fds: list[FD.FunctionalDependency] = []
+
+    # for each user-inputted functional dependency string
+    for fd in fd_list: 
+        if "->->" in fd: # for multi-valued dependencies
+            partitioned_fd = fd.partition("->->")
+        elif "->" in fd: # for regular dependencies
+            partitioned_fd = fd.partition("->")
+        
+        # each attribute split into list element e.g. ["CourseStart", "CourseEnd"]
+        lhs = [x.strip() for x in partitioned_fd[0].split(',')] 
+        rhs = [x.strip() for x in partitioned_fd[2].split(',')]
+        
+        # making each attribute string into Attribute object, add to corresponding set
+        isPrime: bool = False
+        # creating determinants list of Attribute objects
+        for attr in lhs: 
+            if attr in primary_key: # if the current attribute is part of primary key
+                isPrime = True
+            else:
+                isPrime = False
+            determinants.add(A.Attribute(attr, isPrime)) 
+        # creating non-determinants list of Attribute objects
+        for attr in rhs: 
+            if attr in primary_key:
+                isPrime = True
+            else:
+                isPrime = False
+            non_determinants.add(A.Attribute(attr, isPrime)) 
+        
+        # creating FunctionDependency object with determinants and non_determinants, add to list
+        parsed_fds.append(FD.FunctionalDependency(determinants, non_determinants))
+
+    return parsed_fds
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import DatabaseSchema
-import FunctionalDependency
-import Attribute
-import Parse
-
+import FunctionalDependency as FD
+import Attribute as A
+import Parse as P
+import Table as T
+"""
 name: Attribute.Attribute = Attribute.Attribute("name")
 age: Attribute.Attribute = Attribute.Attribute("age")
 primeAttributes: list[Attribute.Attribute] = [name]
@@ -10,22 +11,34 @@ nonPrimeAttributes: list[Attribute.Attribute] = [name]
 functionalDependency: FunctionalDependency.FunctionalDependency = FunctionalDependency.FunctionalDependency(primeAttributes, nonPrimeAttributes)
 print(name)
 print(functionalDependency)
-
+"""
 if __name__ == "__main__":
-    #get input data file and parse data
+
+    # get input data file and parse data to get attributes
     print("Input dataset:")
     file: str = input()
+    attributes: list[A.Attributes] = P.csvParse(file) # list of Attributes used to create Table object
 
+    # get Primary Key of table
+    print("Key (can be composite):")
+    key: str = input()
+    key_set = P.keyParse(key)
+
+    # get functional dependencies and parse determinants and dependents
     print("Input Functional Dependencies (type “exit” and hit enter to complete your dependency list):")
     fd: str = ""
+    fd_list: list[str] = [] # list of inputted fds
+    parsed_fd_list: list[FD.FunctionalDependency] = [] # list of parsed fds
     while fd != "exit":
         fd = input()
+        fd_list.append(fd)
+    parsed_fd_list = P.fdParse(fd_list, key_set) # list of FDs used to create Table object
+
+    # create the Table object with Attributes and FDs
+    table: T.Table = T.Table(attributes, parsed_fd_list)
 
     print("Choice of the highest normal form to reach (1: 1NF, 2: 2NF, 3: 3NF, B: BCNF, 4: 4NF, 5: 5NF):")
     highest_nf: int = int(input())
 
     print("Find the highest normal form of the input table? (1: Yes, 2: No):")
     find_hnf: int = int(input())
-
-    print("Key (can be composite):")
-    key: str = input()
