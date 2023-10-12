e763566c58f139cd3c1920a0a89d1cfa5f9e8bfb
Daniel Martin <dlmfxq@umsystem.edu>
2023-10-12 12:34:34 -0500

    refactor FunctionalDependency.py

--- FunctionalDependency.py
+++ FunctionalDependency.py
@@ -4,16 +4,12 @@ import Attribute
 
 
 class FunctionalDependency:
-    determinants: list[Attribute.Attribute]
-    nonDeterminants: list[Attribute.Attribute]
+    determinants: set(Attribute.Attribute)
+    nonDeterminants: set(Attribute.Attribute)
     
-    def __init__(self, determinant: list[Attribute.Attribute], nonDeterminants: list[Attribute.Attribute]) -> None:
+    def __init__(self, determinant: set(Attribute.Attribute), nonDeterminants: set(Attribute.Attribute),) -> None:
         #Attributes: determinant (left hand side)  nonDeterminants (right hand side) of a fucntional Dependency 
         #Returns:    nothing
-        determinants: set(Attribute.Attribute)
-        nonDeterminants: set(Attribute.Attribute)
-
-    def __init__(self, determinant: set(Attribute.Attribute), nonDeterminants: set(Attribute.Attribute),) -> None:
         self.determinants = determinant
         self.nonDeterminants = nonDeterminants
 
