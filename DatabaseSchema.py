d7ba801403e84a04e999acf9398af7847a3550f3
Pierce Dreiling <piercedreiling@gmail.com>
2023-10-19 15:29:23 -0500

    added no-so-pretty print method for class

--- DatabaseSchema.py
+++ DatabaseSchema.py
@@ -7,7 +7,8 @@ class DatabaseSchema:
     
     def __init__(self, original_table: T.Table, tables: list[T.Table] = []) -> None:
         self.original_table = original_table
-        self.tables = tables.append(self.original_table) # add og input table to list of tables
+        self.tables = tables
+        self.tables.append(self.original_table) # add og input table to list of tables
 
     """ General method to normalize all tables in the schema in-place
         to specified normal form; calls each normalization function for entire DB schema
@@ -49,6 +50,35 @@ class DatabaseSchema:
             print("1NF")
         else:
             print("None. Input table does not satisfy any normal form.")
+
+    """ Pretty print all tables
+        Input: self
+        Output: str
+    """
+    def __str__(self) -> str:
+        output: str = ""
+
+        for table in self.tables:
+            output += '-'*20
+            output += "\nAttributes: "
+            for attr in table.attributes:
+                output += attr.name + ", "
+            output += "\nPrimary Key: { "
+            for attr in table.attributes:
+                if attr.isPrime:
+                    output += attr.name + ", "
+            output += "}\n"
+            output += "\nFunctional Dependencies: \n"
+            for fd in table.functionalDependencies:
+                output += "\t{ "
+                for attr in fd.determinants:
+                    output += attr.name + ", "
+                output += "} -> { "
+                for attr in fd.nonDeterminants:
+                    output += attr.name + ", "
+                output += "}\n\n"
+
+        return output
         
 """ Functions to normalize all tables in DB schema
     Input: the DB schema
@@ -66,8 +96,10 @@ def normalizeTo1NF(databaseSchema: DatabaseSchema) -> list[T.Table]:
     return normalizedDatabaseSchema
 
 def normalizeTo2NF(databaseSchema: DatabaseSchema) -> list[T.Table]:
-    normalizedDatabaseSchema: list[T.Table] = normalizeTo1NF(databaseSchema)
+    normalizedDatabaseSchema: list[T.Table] = []
     normalized_tables: set[T.Table]
+
+    databaseSchema.tables = normalizeTo1NF(databaseSchema)
 
     for table in databaseSchema.tables: # normalize each table in schema to 2NF
         normalized_tables = T.normalizeTo2NF(table) 
@@ -77,8 +109,10 @@ def normalizeTo2NF(databaseSchema: DatabaseSchema) -> list[T.Table]:
     return normalizedDatabaseSchema
 
 def normalizeTo3NF(databaseSchema: DatabaseSchema) -> list[T.Table]:
-    normalizedDatabaseSchema: list[T.Table] = normalizeTo2NF(databaseSchema)
+    normalizedDatabaseSchema: list[T.Table] = []
     normalized_tables: set[T.Table]
+
+    databaseSchema.tables = normalizeTo2NF(databaseSchema)
 
     for table in databaseSchema.tables: # normalize each table in schema to 3NF
         normalized_tables = T.normalizeTo3NF(table) 
@@ -88,8 +122,10 @@ def normalizeTo3NF(databaseSchema: DatabaseSchema) -> list[T.Table]:
     return normalizedDatabaseSchema
 
 def normalizeToBCNF(databaseSchema: DatabaseSchema) -> list[T.Table]:
-    normalizedDatabaseSchema: list[T.Table] = normalizeTo3NF(databaseSchema)
+    normalizedDatabaseSchema: list[T.Table] = []
     normalized_tables: set[T.Table]
+
+    databaseSchema.tables = normalizeTo3NF(databaseSchema)
 
     for table in databaseSchema.tables: # normalize each table in schema to BCNF
         normalized_tables = T.normalizeToBCNF(table) 
@@ -99,8 +135,10 @@ def normalizeToBCNF(databaseSchema: DatabaseSchema) -> list[T.Table]:
     return normalizedDatabaseSchema
 """ Add back in when 4NF and 5NF functions are made
 def normalizeTo4NF(databaseSchema: DatabaseSchema) -> list[T.Table]:
-    normalizedDatabaseSchema: list[T.Table] = normalizeToBCNF(databaseSchema)
+    normalizedDatabaseSchema: list[T.Table] = []
     normalized_tables: set[T.Table]
+
+    databaseSchema.tables = normalizeToBCNF(databaseSchema)
 
     for table in databaseSchema.tables: # normalize each table in schema to 4NF
         normalized_tables = T.normalizeTo4NF(table) 
@@ -110,8 +148,10 @@ def normalizeTo4NF(databaseSchema: DatabaseSchema) -> list[T.Table]:
     return normalizedDatabaseSchema
 
 def normalizeTo5NF(databaseSchema: DatabaseSchema) -> list[T.Table]:
-    normalizedDatabaseSchema: list[T.Table] = normalizeTo4NF(databaseSchema)
+    normalizedDatabaseSchema: list[T.Table] = []
     normalized_tables: set[T.Table]
+
+    databaseSchema.tables = normalizeTo4NF(databaseSchema)
 
     for table in databaseSchema.tables: # normalize each table in schema to 5NF
         normalized_tables = T.normalizeTo5NF(table) 
@@ -120,3 +160,10 @@ def normalizeTo5NF(databaseSchema: DatabaseSchema) -> list[T.Table]:
     
     return normalizedDatabaseSchema
 """
+
+def createSQLQueries(databaseSchema: DatabaseSchema):
+    
+    for table in databaseSchema.tables:
+        pass
+
+    return
